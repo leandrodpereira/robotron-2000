44505933ca5fe0e248a990d1b4318267f752b321
Leandro Duarte <leandrodpereira@gmail.com>
2023-07-05 17:19:22 -0300

    aula01

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const robotron = document.querySelector("#robotron")
+
+robotron.addEventListener("click", (evento)=> {
+    console.log(evento)
+})
+
+function dizOi(nome){
+    console.log("Oi " + nome)
+}
+
+dizOi("Leandro")
+//Para saber mais sobre "Hoisting"
+//https://www.alura.com.br/artigos/hoisting-no-javascript?_gl=1*v6yocy*_ga*NTQwODY5OTU4LjE2NDQ0Mjk4MDY.*_ga_59FP0KYKSM*MTY4ODU4NTIyNy4xMC4xLjE2ODg1ODgwMDYuMC4wLjA.*_fplc*cCUyQjZnc3VnSm1zdWNqSllJV1ZPUFVKVmRLbGtnVjljViUyQkolMkZWRXZVRzBwakxyVkFTVXYzQXM0NjNvamNvRlJtTXJkeUxzJTJGcVNwbEp0M3lMJTJCVFYxTlJTeW1IMklyQ25raUJxSk9DOXZiMmlLeFBOZFRaVkNxMGpLQmgzQThLQSUzRCUzRA..
