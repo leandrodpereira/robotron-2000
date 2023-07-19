343b32ba84a02451f9c5799b3f40b918ab4ed331
Leandro Duarte <leandrodpereira@gmail.com>
2023-07-19 17:12:15 -0300

    aula03

--- js/main.js
+++ js/main.js
@@ -1,13 +1,46 @@
-const robotron = document.querySelector("#robotron")
+// const robotron = document.querySelector("#robotron")
 
-robotron.addEventListener("click", (evento)=> {
-    console.log(evento)
+// robotron.addEventListener("click", (evento)=> {
+//     console.log(evento)
+// })
+
+// function dizOi(nome){
+//     console.log("Oi " + nome)
+// }
+
+// dizOi("Leandro")
+//Para saber mais sobre "Hoisting"
+//https://www.alura.com.br/artigos/hoisting-no-javascript
+
+const somar = document.querySelector("#somar")
+const subtrair = document.querySelector("#subtrair")
+const braco = document.querySelector("#braco")
+
+const controle = document.querySelectorAll(".controle-ajuste")
+//https://www.alura.com.br/artigos/javascript-para-que-serve-array
+
+// somar.addEventListener("click", (evento)=> {
+//     braco.value = parseInt(braco.value) + 1
+// })
+
+// subtrair.addEventListener("click", (evento)=> {
+//     braco.value = parseInt(braco.value) - 1
+// })
+
+// somar.addEventListener("click", (evento)=> {manipulaDados("somar")})
+// subtrair.addEventListener("click", (evento)=> {manipulaDados("subtrair")})
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent)
+    })
 })
 
-function dizOi(nome){
-    console.log("Oi " + nome)
-}
 
-dizOi("Leandro")
-//Para saber mais sobre "Hoisting"
-//https://www.alura.com.br/artigos/hoisting-no-javascript?_gl=1*v6yocy*_ga*NTQwODY5OTU4LjE2NDQ0Mjk4MDY.*_ga_59FP0KYKSM*MTY4ODU4NTIyNy4xMC4xLjE2ODg1ODgwMDYuMC4wLjA.*_fplc*cCUyQjZnc3VnSm1zdWNqSllJV1ZPUFVKVmRLbGtnVjljViUyQkolMkZWRXZVRzBwakxyVkFTVXYzQXM0NjNvamNvRlJtTXJkeUxzJTJGcVNwbEp0M3lMJTJCVFYxTlJTeW1IMklyQ25raUJxSk9DOXZiMmlLeFBOZFRaVkNxMGpLQmgzQThLQSUzRCUzRA..
+function manipulaDados(operacao){
+    if(operacao === "-"){
+        braco.value = parseInt(braco.value) - 1
+    }else {
+        braco.value = parseInt(braco.value) + 1
+    }
+}
